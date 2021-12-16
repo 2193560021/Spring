950d1ef44137278941db2033b948372d27d6fd51
2193560021 <62364555+2193560021@users.noreply.github.com>
2021-12-16 20:45:56 +0800

    第三天

--- spring5_demo1/src/com/lyy/Book.java
+++ spring5_demo1/src/com/lyy/Book.java
@@ -8,6 +8,7 @@ public class Book {
     //创建属性
     private String bname;
     private String author;
+    private String address;
 
     //创建属性对应的set方法
     public void setBname(String bname) {
@@ -18,7 +19,11 @@ public class Book {
         this.author = author;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public void testDemo(){
-        System.out.println(bname + "::" + author);
+        System.out.println(bname + "::" + author + "::" + address);
     }
 }
--- spring5_demo1/src/com/lyy/testdemo/TestSpring5.java
+++ spring5_demo1/src/com/lyy/testdemo/TestSpring5.java
@@ -1,6 +1,7 @@
 package com.lyy.testdemo;
 
 import com.lyy.Book;
+import com.lyy.Orders;
 import com.lyy.User;
 import javafx.application.Application;
 import org.junit.Test;
@@ -40,6 +41,18 @@ public class TestSpring5 {
         Book book = context.getBean("book", Book.class);
         System.out.println(book);
         book.testDemo();
+    }
+
+
+    @Test
+    public void testOrder(){
+        //1 加载spring配置文件
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean1.xml");
+        //2 获取配置创建的对象
+        Orders orders = context.getBean("orders", Orders.class);
+        System.out.println(orders);
+        orders.ordersTest();
     }
 
 
